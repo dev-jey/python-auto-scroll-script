462b00edd254605eb0beb549bd1fabe0dcb63122
dev-jey <savalijey@gmail.com>
2021-02-10 15:00:48 +0300

    Initial commit

---
+++ scroller.py
@@ -0,0 +1,11 @@
+import pyautogui
+from time import sleep
+​
+print(">>>>>>>>>>>>>>>>> Working >>>>>>>>>>>>>>>")
+def run_gui():
+	pyautogui.moveTo(100, 150)
+	pyautogui.moveRel(0,10)
+​
+while True:
+	run_gui()
+	sleep(2)
