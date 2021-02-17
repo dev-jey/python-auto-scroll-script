038acb38cef04bf7c4f2a66e93c088bbfe4644d4
dev-jey <savalijey@gmail.com>
2021-02-17 07:42:36 +0300

    Fix heroku process

--- scroller.py
+++ scroller.py
@@ -2,10 +2,13 @@ import pyautogui
 from time import sleep
 
 print(">>>>>>>>>>>>>>>>> Working >>>>>>>>>>>>>>>")
+
+
 def run_gui():
-	pyautogui.moveTo(100, 150)
-	pyautogui.moveRel(0,10)
+    pyautogui.moveTo(100, 150)
+    pyautogui.moveRel(0, 10)
+
 
 while True:
-	run_gui()
-	sleep(2)
+    run_gui()
+    sleep(3)
